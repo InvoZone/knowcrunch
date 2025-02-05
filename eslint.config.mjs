2012385c62ed35bf3bda529fca82597f33d96da3
mudasser <mudasser.hussain@invozone.com>
2025-02-05 15:56:12 +0500

    eslint and prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,29 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [...compat.config({
+  extends: ["next/core-web-vitals", "next", "prettier"],
+  rules: {
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "double"],
+    "semi": ["error"],
+    "no-unused-vars": ["error"],
+    "no-console": ["warn"],
+    "eqeqeq": ["error", "always"],
+    "react/prop-types": "off",
+    "react/jsx-key": "error",
+    "react/no-direct-mutation-state": "error",
+    "prefer-arrow-callback": ["error"],
+    "prefer-template": ["error"],
+    "react/no-unused-state": "warn",
+    "react/no-unknown-property": "warn",
+    "react/no-array-index-key": "warn"
+  }
+
+})
+
+];
+
 
 export default eslintConfig;
