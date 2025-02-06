11456d9f4b23ebb24834a91c34a0279e7e0611b4
Mohsin Ali Raza <mohsinaliraza@192.168.10.4>
2025-02-06 11:57:30 +0500

    redux toolkit added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Foco } from "@/lib/fonts";
 import "./globals.css";
+import Providers from "@/store/Provider";
 
 export const metadata = {
   title: "KnowCrunch",
@@ -8,10 +9,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={Foco.className}>
-        {children}
-      </body>
-    </html>
+    <Providers>
+      <html lang="en">
+        <body className={Foco.className}>{children}</body>
+      </html>
+    </Providers>
   );
 }
---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import loading from "../store/slices/loading";
+
+export const store = configureStore({
+  reducer: {
+    loading,
+    // Add your reducers here
+    // yourReducer: yourReducer,
+  },
+});
