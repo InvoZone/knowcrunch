6d19c8cee9b858f7e324b33ed62aeb98621866a6
mudasser <mudasser.hussain@invozone.com>
2025-02-12 17:17:52 +0500

    separate page heading

---
+++ src/components/common/pageHeadingDesc.js
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+
+const PageHeadingDesc = ({ heading, subHeading }) => {
+    return (
+        <>
+            {/* Main content header */}
+            <Typography variant="h2" color="base1.dark4">{heading}</Typography>
+            <Typography variant="titleMd" color="base1.dark4">{subHeading}</Typography>
+        </>
+    );
+};
+
+export default PageHeadingDesc;
