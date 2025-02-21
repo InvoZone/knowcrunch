5c3ed2301fe33e3c412139c9ea5a41defa78d6bc
Mohsin Ali Raza <mohsinaliraza@192.168.10.4>
2025-02-21 23:58:45 +0500

    animation styling

--- src/components/common/logoAnimate.js
+++ src/components/common/logoAnimate.js
@@ -31,13 +31,44 @@ const LogoAnimate = () => {
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
             maxWidth="lg"
-            sx={{ padding: 0 }}
+            sx={{ padding: 0, position: "relative" }}
             className={styles.mainconatiner}
         >
             <Box
-                className={styles.LogoAnimate}
                 sx={{
-                    padding: "4px 48px",
+                    width: "82px",
+                    height: "36px",
+                    background:
+                        "linear-gradient(90deg, rgb(255 255 255) 87%, rgba(255, 255, 255, 0.563484768907563) 100%)",
+                    position: "absolute",
+                    left: {
+                        xs: "30px",
+                        sm: "48px",
+                        md: "50px",
+                        lg: "60px",
+                    },
+                    top: "12px",
+                    zIndex: 2,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <Typography
+                    variant="body"
+                    sx={{ width: "90px", color: base1.dark4 }}
+                >
+                    Trusted by
+                </Typography>
+            </Box>
+            <Box
+                sx={{
+                    padding: {
+                        xs: "4px 8px 4px 48px",
+                        sm: "4px 24px",
+                        md: "4px 48px",
+                        lg: "4px 48px",
+                    },
                     display: "flex",
                     background: "white",
                     alignItems: "center",
@@ -46,13 +77,11 @@ const LogoAnimate = () => {
                     borderRadius: "48px",
                 }}
             >
-                <Typography variant="body1" sx={{ width: "90px", color: base1.dark4 }}>
-                    Trusted by
-                </Typography>
-                <Marquee 
-                    play={play} 
-                    speed={130} 
-                    direction={direction} 
+                <Marquee
+                    play={play}
+                    speed={130}
+                    direction={direction}
+                    style={{ paddingLeft: "24px", marginLeft: "40px" }}
                     onCycleComplete={onCycleComplete}
                     // loop={1} // Stop after reaching the end
                 >
@@ -68,8 +97,25 @@ const LogoAnimate = () => {
                         "/icons/sarantislogo.svg",
                         "/icons/trainedlogo.svg",
                     ].map((item, index) => (
-                        <Box key={index} sx={{ padding: "0 24px" }}>
-                            <Image width={50} height={50} loading="lazy" src={item} alt={`logo-${index}`} />
+                        <Box
+                            key={index}
+                            sx={{
+                                padding: {
+                                    xs: "0 4px",
+                                    sm: "0 8px",
+                                    md: "0 16px",
+                                    lg: "0 16px",
+                                },
+                                ml: 2,
+                            }}
+                        >
+                            <Image
+                                width={50}
+                                height={50}
+                                loading="lazy"
+                                src={item}
+                                alt={`logo-${index}`}
+                            />
                         </Box>
                     ))}
                 </Marquee>
