4ac5ee783e0019d6e7d70cebad7e187c74fcdc13
Innayatullahh <innayat.dev02@invo.email>
2025-03-28 15:17:09 +0500

    svg issue

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,33 @@
-import type { NextConfig } from 'next';
+// import type { NextConfig } from 'next';
+// import createNextIntlPlugin from 'next-intl/plugin';
+
+// const nextConfig: NextConfig = {
+//   experimental: {
+//     turbo: {
+//       rules: {
+//         '*.svg': {
+//           loaders: ['@svgr/webpack'],
+//           as: '*.tsx'
+//         }
+//       }
+//     }
+//   }
+// };
+
+// const withNextIntl = createNextIntlPlugin();
+// export default withNextIntl(nextConfig);
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.tsx'
-        }
-      }
-    }
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config: { module: { rules: { test: RegExp; use: string[]; }[]; }; }) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
