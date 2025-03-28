ef15ba310ba0ec6bc17bb656fe7fd24f48ea6b5b
Innayatullahh <innayat.dev02@invo.email>
2025-03-28 15:17:37 +0500

    remove commented code

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,4 @@
-// import type { NextConfig } from 'next';
-// import createNextIntlPlugin from 'next-intl/plugin';
 
-// const nextConfig: NextConfig = {
-//   experimental: {
-//     turbo: {
-//       rules: {
-//         '*.svg': {
-//           loaders: ['@svgr/webpack'],
-//           as: '*.tsx'
-//         }
-//       }
-//     }
-//   }
-// };
-
-// const withNextIntl = createNextIntlPlugin();
-// export default withNextIntl(nextConfig);
 import createNextIntlPlugin from 'next-intl/plugin';
 
 /** @type {import('next').NextConfig} */
